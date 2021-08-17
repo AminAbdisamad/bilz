c9c908f38b49d88cd739eeafebfa6536f6710363
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-08-18 02:30:57 +0300

    Cleaned addItem struct func

---
+++ bill.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+
+type Bill struct {
+	name string
+	items map[string]float64
+	tip float64
+
+}
+
+func newBill(name string)Bill {
+	nb := Bill{
+		name:name,
+		items:map[string]float64{
+			"coffee":5.99,
+			"tea":3.89,
+		},
+		tip:0.0,
+	}
+
+	return nb
+}
+
+// Receiver function : are functions related to structs or interfaces
+func (b Bill) format()string{
+	var total float64 = 0
+	var str = "Your bills are\n"
+	for key,value := range b.items{
+		str += fmt.Sprintf("%-25v ...$%v\n", key+":",value)
+		total += value
+	}
+
+	str += fmt.Sprintf("%-25v ...$%v\n","Tip:",b.tip)
+	str += fmt.Sprintf("%-25v ...$%0.2f\n","Total:",total + b.tip)
+	return str
+
+}
+
+func (b *Bill) updateTip(tip float64){
+	b.tip = tip
+}
+
+func (b Bill) addItem(name string, price float64){
+	b.items[name] = price
+
+}
--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello there")
+	bill := newBill("Mohamed")
+	bill.updateTip(10)
+	bill.addItem("Milk",3.44)
+	bill.addItem("Soup",5.48)
+	bill.addItem("Yogurt",3.94)
+	fmt.Println(bill.format())
 }
